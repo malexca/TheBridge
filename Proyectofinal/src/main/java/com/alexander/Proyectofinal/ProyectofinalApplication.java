b0dc864cf4b506abb899f659d11c92a954271eb9
alex <alex@localhost>
2024-01-30 11:41:01 +0100

    Añadir ComponentScan, EntityScan & EnableJpa

--- Proyectofinal/src/main/java/com/alexander/Proyectofinal/ProyectofinalApplication.java
+++ Proyectofinal/src/main/java/com/alexander/Proyectofinal/ProyectofinalApplication.java
@@ -2,8 +2,14 @@ package com.alexander.Proyectofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.alexander")
+@EntityScan("com.alexander.models")
+@EnableJpaRepositories("com.alexander.repositories")
 public class ProyectofinalApplication {
 
 	public static void main(String[] args) {
